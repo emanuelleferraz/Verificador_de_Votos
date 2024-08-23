3eea54009642176290a5bf894735caa52d4e1b3c
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2024-08-23 16:36:49 -0300

    Crud de Deputados e Projetos
    Implementação do crud de Deputados e Projetos

--- database/databaseDeputados.py
+++ database/databaseDeputados.py
@@ -9,3 +9,41 @@ db = client.db('informations_Deputados_and_Projetos', username='root', password=
 # Selecionar a coleção
 collection = db.collection('deputados')
 
+def insertDeputado(name, party, number): 
+    document = {
+    "record": number,
+    "name": name,
+    "party": party,
+    }
+    collection.insert(document)
+
+def deleteDeputado(name):
+    deputado_to_delete = findDeputadoByName(name)
+    key_to_delete = deputado_to_delete["_key"]
+    collection.delete(key_to_delete)
+
+def findDeputadoByName(name):
+    deputado = collection.find({"nome":name})
+    return deputado
+
+def findDeputadoByRecord(number):
+    deputado = collection.find({"record":number})
+    return deputado
+
+def updateDeputadoByName(party, name):
+    deputado_to_update = findDeputadoByName(name)
+    key_to_update = deputado_to_update["_key"]
+    document_to_update = {
+        "_key": key_to_update,
+        "party": party
+    }
+    collection.update(document_to_update)
+
+def updateDeputadoByRecord(party, number):
+    deputado_to_update = findDeputadoByRecord(number)
+    key_to_update = deputado_to_update["_key"]
+    document_to_update = {
+        "_key": key_to_update,
+        "party": party
+    }
+    collection.update(document_to_update)
--- database/databaseProjetos.py
+++ database/databaseProjetos.py
@@ -8,3 +8,42 @@ db = client.db('informations_Deputados_and_Projetos', username='root', password=
 
 # Selecionar a coleção
 collection = db.collection('projetos')
+
+def insertLaw(name, description, number): #coloquei nome e party so pra criar a function
+    document = {
+    "lawNumber": number,
+    "nome": name,
+    "description": description,
+    }
+    collection.insert(document)
+
+def deleteLaw(name):
+    law_to_delete = findLawByName(name)
+    key_to_delete = law_to_delete["_key"]
+    collection.delete(key_to_delete)
+
+def findLawByName(name):
+    law = collection.find({"nome":name})
+    return law
+
+def findLawByNumber(number):
+    law = collection.find({"lawNumber":number})
+    return law
+
+def updateLawByName(description, name):
+    law_to_update = findLawByName(name)
+    key_to_update = law_to_update["_key"]
+    document_to_update = {
+        "_key": key_to_update,
+        "description": description
+    }
+    collection.update(document_to_update)
+
+def updateLawByNumber(description, number):
+    law_to_update = findLawByNumber(number)
+    key_to_update = law_to_update["_key"]
+    document_to_update = {
+        "_key": key_to_update,
+        "description": description
+    }
+    collection.update(document_to_update)
