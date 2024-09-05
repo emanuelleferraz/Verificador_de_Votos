8ecb7302fd76e7f786091891734e0ae5ff0ccdb8
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2024-09-05 15:46:21 -0300

    atualizações no front

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ def deputados():
     deputados_list = findAllDeputados()
     return render_template("deputados.html", deputados=deputados_list)
 
+@app.route("/projeto_leis")
+def projeto_leis():
+    return render_template("projetoLeis.html")
+
+@app.route("/conexoes")
+def conexoes():
+    return render_template("conexoes.html")
+    
 @app.route("/login")
 def login():
     return render_template("login.html")
 
 if __name__ in "__main__":
     app.run(debug=True, port=20000)
-
-
